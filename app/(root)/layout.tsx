354ae86c31404c9e5104c00251b72138ace30c8d
Siddhant Trivedi <siddhanttrivedi56@gmail.com>
2024-09-21 22:00:49 +0530

    Implement complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,29 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>sidebar {children}</main>;
+  const loggeddIn = {
+    firstName: "Siddhant",
+    lastName: "Trivedi",
+  };
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggeddIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggeddIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Siddhant",
+    lastName: "Trivedi",
+    email: "contact@sid",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access And Manage Your Account And Transactions Efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
